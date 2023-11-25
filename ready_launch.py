5298af55459c1443cfb027ada0715f028de8a4f3
akhil4674 <119128200+akhil4674@users.noreply.github.com>
2023-11-25 13:03:39 +0100

    Update ready_launch.py

--- ready_launch.py
+++ ready_launch.py
@@ -1,84 +1,49 @@
-import rclpy
-import time
-import launch
-import unittest
 import pytest
-from launch import LaunchDescription
+import rclpy
+from std_msgs.msg import Bool
+from sensor_msgs.msg import JointState
+from your_package_name.joint_state_subscriber import JointStateSubscriber
 
+@pytest.fixture
+def joint_state_subscriber():
+    rclpy.init()
+    node = JointStateSubscriber()
+    yield node
+    node.destroy_node()
+    rclpy.shutdown()
 
-def ReadyToTest():
-    pass
+def test_arm_status_evaluation(joint_state_subscriber):
+    """Integration test for JointStateSubscriber"""
+    # Create a JointState message with arm in stowed position and gripper open
+    joint_states_arm_stowed_gripper_open = JointState()
+    joint_states_arm_stowed_gripper_open.name = [
+        "arm_sh0", "arm_sh1", "arm_hr0", "arm_el0", "arm_el1", "arm_wr0", "arm_wr1", "arm_f1x"
+    ]
+    joint_states_arm_stowed_gripper_open.position = [
+        -0.0001456737518310547, -3.115017890930176, 0.0, 3.132694959640503, 1.570010781288147,
+        0.0025632381439208984, -1.56974196434021, -0.04  # Assuming gripper open position
+    ]
 
-
-def action_relay():
-    pass
-
-
-@pytest.mark.launch_test
-def generate_test_description():
-    test_server = Node(
-        package="ros2_spot_arm_status",
-        executable="spot_arm_status_pub.py",
-        output="screen",
-    )
-
-    return LaunchDescription(
-        [ReadyToTest(), test_server, action_relay()]
-    )
-
-
-def test_arm_status_evaluation(self):
     # Create status callback to verify result
     arm_stowed_msg = None
 
     def arm_status_cb(msg):
         nonlocal arm_stowed_msg
         arm_stowed_msg = msg
-        # Process the received message here
-        if arm_stowed_msg.data:
-            print("Arm is stowed")
-        else:
-            print("Arm is not stowed")
 
-    arm_status_sub = self.node.create_subscription(
+    arm_status_sub = joint_state_subscriber.create_subscription(
         Bool,
-        "/spot/arm/is_stowed",
+        "/gripper_open",
         arm_status_cb,
         rclpy.qos.qos_profile_system_default,
     )
 
     # Publish joint state
-    joint_state_msg = JointState()
-    joint_state_msg.name = [
-        "arm_sh0",
-        "arm_sh1",
-        "arm_hr0",
-        "arm_el0",
-        "arm_el1",
-        "arm_wr0",
-        "arm_wr1",
-    ]
-    joint_state_msg.position = [
-        -0.0001456737518310547,
-        -3.115017890930176,
-        0.0,
-        3.132694959640503,
-        1.570010781288147,
-        0.0025632381439208984,
-        -1.56974196434021,
-    ]
-
-    self.node.publish("/joint_states", joint_state_msg)
+    joint_state_subscriber.joint_state_callback(joint_states_arm_stowed_gripper_open)
 
     # Wait until the status has been evaluated
-    arm_status_received = False
     end_time = time.time() + 5
     while arm_stowed_msg is None and time.time() < end_time:
-        rclpy.spin_once(self.node, timeout_sec=0.1)
+        rclpy.spin_once(joint_state_subscriber, timeout_sec=0.1)
 
-    self.assertTrue(arm_stowed_msg.data)
-
-    # Use arm_status_sub and arm_status_received here if needed
-    # ...
-
-    return LaunchDescription([ReadyToTest(), test_server, action_relay()])
+    assert arm_stowed_msg.data
